70ae24e1c3d5ed0adfcb2db193c36d0cf04ef3ab
Etienne Boursault <43215408+EhBoss@users.noreply.github.com>
2022-02-12 22:36:15 +0100

    Gestion des erreurs

--- javascript/script.js
+++ javascript/script.js
@@ -1,50 +1,44 @@
-const compare = function(id, asc) {
-    // les "row 1" & "row 2" sont bien liés à ma fonction sort().
-    return function(row1, row2) {
-        // Retourne la valeur de la cellule
-        const tdValue = function (row, id) {
-            return row.children[id].textContent;
-        }
+use strict;
 
-        // La fonction tri compare les valeurs v1 et v2 issues des tdValues et retourne les valeurs -1, 0 ou 1
-        const tri = function (v1, v2) {
-            if (v1 !== '' && v2 !== !isNaN(v1) && !isNaN(v2)){
-                return v1 - v2;
-            } else {
-                console.log('tri --- v1 : ' + v1 + ' - v2 : ' + v2 + ' => v1.toString().localeCompare(v2) : ' + v1.toString().localeCompare(v2));
-                return v1.toString().localeCompare(v2);
-            }
-        };
-        return tri(tdValue(asc ? row1 : row2, id), tdValue(asc ? row2 : row1, id));
+// Gestion des erreurs
+// Gérer une erreur avec les blocs try…catch
+let p1 = document.getElementById('p1');
+try {
+    prenom;
+    p1.innerHTML = 'Bonjour';
+}catch (err) {
+    p1.innerHTML =
+        'Erreur rencontrée.' +
+        '<br>Nom de l\'erreur : ' + err.name +
+        '<br>Message de l\'erreur : ' + err.message +
+        '<br>Emplacement de l\'erreur : ' + err.stack;
+}
+
+// Les exceptions en JavaScript et l’instruction throw
+let p2 = document.getElementById('p2');
+function div() {
+    let x = prompt('Entrez un premier nombre (numérateur)');
+    let y = prompt('Entrez un premier nombre (dénominateur)');
+
+    if (isNaN(x) || isNaN(y) || x == '' || y == '') {
+        throw new Error('Merci de rentrer 2 nombres');
+    } else if (y == 0) {
+        throw new Error('Division par 0 impossible');
+    } else {
+        p2.innerHTML = x/y;
     }
 }
 
-const tbody = document.querySelector('tbody');
-const thx = document.querySelectorAll('th');
-const trxb = document.querySelectorAll('tbody tr');
+let p3 = document.getElementById('p3');
+try {
+    div();
+} catch (err) {
+    p2.innerHTML = err.message;
+}
+// Le bloc finally
+finally {
+    p3.innerHTML = 'Ce message s\'affiche quoiqu\'il arrive';
+}
 
-thx.forEach(function(th) {
-    th.addEventListener('click', function () {
-        // Array.from(thx).indexOf(th) - Donne le numéro de la colonne du tableau et la trie
-        arrayC = Array.from(thx).indexOf(th);
-        console.log('th.addEventListener --- arrayC : ' + arrayC + ' - arrayC.valueOf() : ' + arrayC.valueOf());
-        ascV = !this.asc;
-        console.log('th.addEventListener --- ascV : ' + ascV);
-        compareV = compare(arrayC, ascV);
-        console.log('th.addEventListener --- compareV : ' + compareV);
-        triC = Array.from(trxb).sort(compareV);
-        triC.forEach(function (tr){
-            console.log('th.addEventListener --- triC ; tr' + Array.from(trxb).indexOf(tr)  + ' : ' + tr.textContent);
-        })
-        // Array.from(thx).indexOf(th) récupère l'id de la colonne à trier
-        // Compare applique les fonctions tdValue et tri à chaque "th"
-        // La fonction sort() => Dans la fonction compare, le retour est égale à -1, 0 ou 1 et la fonction sort() se sert de
-        // ce retour pour agencer ces 2 valeurs selon le second paramètre, "true" pour croissant et "false" pour décroissant
-        // ex [4, 2, 5, 1, 3].sort((a, b) => a - b); https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-        let classe = Array.from(trxb).sort(compare(Array.from(thx).indexOf(th), this.asc = !this.asc));
-        // Reconstruit l'ordre de la colonne
-        classe.forEach(function (tr) {
-            tbody.appendChild(tr);
-        });
-    })
-});
+// Le mode script
+// Activer et utiliser le mode strict
